760804f8f58a35170bf4f17c9dbd3e20ef5f016d
walter fedyna <fedynagkjnr@gmail.com>
2025-11-28 07:01:58 +0300

    Add initial PHP application welcome page

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+// Simple test to confirm PHP + Apache is working
+echo "<h1>Welcome to My PHP Application</h1>";
+echo "<p>Your server is running correctly!</p>";
+echo "<p>Current Date/Time: " . date("Y-m-d H:i:s") . "</p>";
+?>
